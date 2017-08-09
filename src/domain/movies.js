fcd8764e86fec1c49623af09990b992fda78359f
Xavier Serrano <Xavier Serrano>
2017-08-09 20:34:31 +0200

    add firt page to movie

--- src/domain/movies.js
+++ src/domain/movies.js
@@ -1,6 +1,27 @@
 import {
     Observable
 } from "rx"
+
+import Observables from '../lib/observables'
+
+
+const getMoviesList = ($) =>{
+   return $('ul.pelilist').children('li').map(function(i, elem){
+       const parent = $(this).children('a')
+       return {
+            title: parent.find('h2').first().text().trim(),
+            link: parent.attr('href'),
+            image_url: parent.find('img').first().attr('src'),
+            format: parent.find('span').first().text() 
+       }
+    }).get()
+}
+
+
 module.exports = function ($) {
-    return Observable.just({})
+    return Observable.just($)
+                    .map(getMoviesList)
+                    .flatMap(Observable.fromArray)
+                    .flatMap()
+                    
 }
--- src/lib/strings.js
+++ src/lib/strings.js
@@ -1,11 +1,11 @@
 import R from "ramda"
 
-export const pretty = function (strings, ...values){
+export const pretty = function (strings, ...values) {
     const tempValues = R.clone(values)
-    return strings.reduce((memo, item)=>{
-        const rest = tempValues.splice(0,1)
-        memo+=item;
-        if(rest.length>0) {
+    return strings.reduce((memo, item) => {
+        const rest = tempValues.splice(0, 1)
+        memo += item;
+        if (rest.length > 0) {
             memo += JSON.stringify(rest[0], null, " ")
         }
         return memo;
