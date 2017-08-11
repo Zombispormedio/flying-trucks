df51c1f21de381eaf68d9c38402be63945721dd7
Xavier Serrano <Xavier Serrano>
2017-08-11 18:24:03 +0200

    add ava

--- test/index.js
+++ test/index.js
@@ -1,12 +1,9 @@
+import test from 'ava';
 import {Observable} from 'rx'
 
-Observable.zip(Observable.range(0, 10), Observable.interval(1000)).subscribe(
-    function (x) {
-        console.dir(x);
-    },
-    function (err) {
-        console.log('Error: ' + err);
-    },
-    function () {
-        console.log('Completed');
-    });
+
+test(t => {
+    t.plan(10)
+    return Observable.range(0, 10).map(()=>t.pass())
+        .subscribe()
+})
