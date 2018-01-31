55ea086d3db78eca18cac306f6903fb86033fe60
Xavier Serrano <zombispormedio007@gmail.com>
2018-01-31 23:13:58 +0100

    ADD CONTAINER

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task("deploy", () => {
         [
           `wt create --token ${
             process.env.WT_TOKEN
-          } --secrets-file .deploy.env ./build/min/omnibus.bundle.min.js`
+          } -c ${WT_CONTAINER} --secrets-file .deploy.env ./build/min/omnibus.bundle.min.js`
         ],
         { verbose: true }
       )
